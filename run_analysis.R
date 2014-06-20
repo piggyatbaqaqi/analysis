6b18aa7459eff4e1a0d76cddfca9024b5ee4fe5b
La Monte Henry Piggy Yarroll <piggy+github@google.com>
2014-06-19 22:20:20 -0400

    Add descriptive names for activities.

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,22 @@
 # You should create one R script called run_analysis.R that does the following.
+
+# Get the descriptive activity names.
 activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt",
                               header=FALSE,
 			      col.names=c("activity.code", "activity"))
+# Get the names of columns.
 features <- read.table("UCI HAR Dataset/features.txt",
                        header=FALSE,
 		        col.names=c("featurecode", "feature"))
 
+# Load the training set.
 train.subject <- read.table("UCI HAR Dataset/train/subject_train.txt",
                             header=FALSE, col.names=c("subject"))
 train.y       <- read.table("UCI HAR Dataset/train/y_train.txt",
                             header=FALSE, col.names=c("activity.code"))
 train.X       <- read.table("UCI HAR Dataset/train/X_train.txt",
                             header=FALSE, col.names=features$feature)
-
+# Load the test set.
 test.subject  <- read.table("UCI HAR Dataset/test/subject_test.txt",
                             header=FALSE, col.names=c("subject"))
 test.y        <- read.table("UCI HAR Dataset/test/y_test.txt",
@@ -21,13 +25,17 @@ test.X        <- read.table("UCI HAR Dataset/test/X_test.txt",
                             header=FALSE, col.names=features$feature)
 
 # Merge the training and the test sets to create one data set.
-ucihar <- rbind(train.X, test.X)
-ucihar$subject <- rbind(train.subject, test.subject)
-ucihar$activity <- rbind(train.y, test.y)
+ucihar.raw <- rbind(train.X, test.X)
 
 # Extract only the measurements on the mean and standard deviation for each measurement. 
+ucihar <- ucihar.raw[, grep("[a-zA-Z]+\\.(std|mean)\\.", names(ucihar.raw))]
 
-# Use descriptive activity names to name the activities in the data set
+# Use descriptive activity names to name the activities in the data set.
+activity.by.code <- rbind(train.y, test.y)
+activity.by.name <- merge(activity.labels, activity.by.code, by="activity.code")
+ucihar$activity <- activity.by.name$activity
+
+ucihar$subject <- rbind(train.subject, test.subject)
 
 # Appropriately label the data set with descriptive variable names. 
 
