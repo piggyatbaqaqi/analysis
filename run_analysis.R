c7860be3e59962ed03fece9b478a85afd49f44e7
La Monte Henry Piggy Yarroll <piggy+github@google.com>
2014-06-21 16:35:20 -0400

    Implement construction of the tidy dataset.

--- run_analysis.R
+++ run_analysis.R
@@ -37,7 +37,8 @@ ucihar.subset$subject <- rbind(train.subject, test.subject)$subject
 
 # Use descriptive activity names to name the activities in the data set.
 ucihar.subset$activity.code <- rbind(train.y, test.y)$activity.code
-ucihar <- merge(activity.labels, ucihar, by="activity.code", all=TRUE)
+ucihar <- merge(activity.labels, ucihar.subset, by="activity.code", all=TRUE)
+ucihar$activity.code <- NULL
 
 # Appropriately label the data set with descriptive variable names. 
 new.names <- names(ucihar)
@@ -51,3 +52,16 @@ names(ucihar) <- new.names
 
 # Create a second, independent tidy data set with the average of each
 # variable for each activity and each subject.
+subject <- ucihar$subject
+activity <- ucihar$activity
+ucihar$subject <- NULL
+ucihar$activity <- NULL
+tidy <- aggregate(ucihar,
+                  by=list(subject=subject, activity=activity),
+                  mean)
+new.names <- paste0("Mean.", names(tidy))
+new.names <- gsub("Mean\\.subject", "subject", new.names)
+new.names <- gsub("Mean\\.activity", "activity", new.names)
+names(tidy) <- new.names
+
+write.table(tidy, file="UCI_HAR_tidy.txt", row.names=FALSE)
